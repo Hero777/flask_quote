981870d81c5b3ea34e1bca518a5872946a51ff74
Hero777 <clickker@mail.ru>
2021-06-24 20:44:45 +0300

    каркас приложения с готовым простым генератором цитат

--- app.py
+++ app.py
@@ -1,11 +1,30 @@
-from flask import Flask
+from flask import Flask, render_template
+from data import data
+
+import random
 
 app = Flask(__name__)
 
 
 @app.route("/")
-def hello_world():
-    return "<p>Hello, World!</p>"
+def index():
+    return render_template('index.html')
+
+
+@app.route("/blog")
+def blog():
+    return render_template('blog.html')
+
+
+@app.route("/quote")
+def quote():
+    citation = random.choice(data)
+    return render_template('quote.html', citation=citation)
+
+
+@app.route("/contacts")
+def contacts():
+    return render_template('contacts.html')
 
 
 if __name__ == '__main__':
